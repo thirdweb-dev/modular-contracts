48fb8c4579a8b219a07abd0866e39f1663ccf5a2
Krishang <krishang@thirdweb.com>
2024-01-11 18:37:19 +0530

    Add comments and move out errors for ERC721Core

--- src/erc721/ERC721Core.sol
+++ src/erc721/ERC721Core.sol
@@ -3,18 +3,13 @@ pragma solidity ^0.8.0;
 
 import {IERC721Metadata} from "../interface/erc721/IERC721Metadata.sol";
 import { IERC2981 } from "../interface/eip/IERC2981.sol";
+import {IERC721CoreCustomErrors} from "../interface/erc721/IERC721CoreCustomErrors.sol";
 import {ERC721Initializable} from "./ERC721Initializable.sol";
 import {NFTHookConsumer} from "../extension/NFTHookConsumer.sol";
 import {Initializable} from "../extension/Initializable.sol";
 import {Permission} from "../extension/Permission.sol";
 
-contract ERC721Core is Initializable, ERC721Initializable, NFTHookConsumer,  Permission {
-
-    /*//////////////////////////////////////////////////////////////
-                               ERRROR 
-    //////////////////////////////////////////////////////////////*/
-
-    error ERC721CoreMintNotAuthorized();
+contract ERC721Core is Initializable, ERC721Initializable, NFTHookConsumer,  Permission, IERC721CoreCustomErrors {
 
     /*//////////////////////////////////////////////////////////////
                     CONSTRUCTOR + INITIALIZE
@@ -24,6 +19,12 @@ contract ERC721Core is Initializable, ERC721Initializable, NFTHookConsumer,  Per
         _disableInitializers();
     }
 
+    /**
+     *  @notice Initializes the ERC-721 Core contract.
+     *  @param _defaultAdmin The default admin for the contract.
+     *  @param _name The name of the token collection.
+     *  @param _symbol The symbol of the token collection.
+     */
     function initialize(address _defaultAdmin, string memory _name, string memory _symbol) external initializer {
         __ERC721_init(_name, _symbol);
         _setupRole(_defaultAdmin, ADMIN_ROLE_BITS);
@@ -70,9 +71,14 @@ contract ERC721Core is Initializable, ERC721Initializable, NFTHookConsumer,  Per
     }
 
     /*//////////////////////////////////////////////////////////////
-                        PERMISSIONED FUNCTIONS
+                        EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
+    /**
+     *  @notice Burns an NFT.
+     *  @dev Calls the beforeBurn hook. Skips calling the hook if it doesn't exist.
+     *  @param _tokenId The token ID of the NFT to burn.
+     */
     function burn(uint256 _tokenId) external {
         address owner = ownerOf(_tokenId);
         if (owner != msg.sender) {
@@ -83,26 +89,42 @@ contract ERC721Core is Initializable, ERC721Initializable, NFTHookConsumer,  Per
         _burn(_tokenId);
     }
 
-    function mint(address _to, uint256 _quantity, bytes memory _data) external payable {
-        (bool success, uint256 tokenIdToMint) = _beforeMint(_to, _quantity, _data);
+    /**
+     *  @notice Mints a token. Calls the beforeMint hook.
+     *  @dev Reverts if beforeMint hook is absent or unsuccessful.
+     *  @param _to The address to mint the token to.
+     *  @param _quantity The quantity of tokens to mint.
+     *  @param _encodedBeforeMintArgs ABI encoded arguments to pass to the beforeMint hook.
+     */
+    function mint(address _to, uint256 _quantity, bytes memory _encodedBeforeMintArgs) external payable {
+        (bool success, uint256 tokenIdToMint) = _beforeMint(_to, _quantity, _encodedBeforeMintArgs);
 
         if (success) {
             _mint(_to, tokenIdToMint, _quantity);
             return;
         }
 
-        revert ERC721CoreMintNotAuthorized();
+        revert ERC721CoreMintingDisabled();
     }
 
-    /*//////////////////////////////////////////////////////////////
-                            OVERRIDE FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
+    /**
+     *  @notice Transfers ownership of an NFT from one address to another.
+     *  @dev Overriden to call the beforeTransfer hook. Skips calling the hook if it doesn't exist.
+     *  @param _from The address to transfer from
+     *  @param _to The address to transfer to
+     *  @param _id The token ID of the NFT
+     */
     function transferFrom(address _from, address _to, uint256 _id) public override {
         _beforeTransfer(_from, _to, _id);
         super.transferFrom(_from, _to, _id);
     }
 
+    /**
+     *  @notice Approves an address to transfer a specific NFT. Reverts if caller is not owner or approved operator.
+     *  @dev Overriden to call the beforeApprove hook. Skips calling the hook if it doesn't exist.
+     *  @param _spender The address to approve
+     *  @param _id The token ID of the NFT
+     */
     function approve(address _spender, uint256 _id) public override {
         _beforeApprove(msg.sender, _spender, _id);
         super.approve(_spender, _id);
@@ -112,6 +134,7 @@ contract ERC721Core is Initializable, ERC721Initializable, NFTHookConsumer,  Per
                             INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
+    /// @dev Returns whether the given caller can update hooks.
     function _canUpdateHooks(address _caller) internal view override returns (bool) {
         return hasRole(_caller, ADMIN_ROLE_BITS);
     }
---
+++ src/interface/erc721/IERC721CoreCustomErrors.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity ^0.8.0;
+
+interface IERC721CoreCustomErrors {
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted on an attempt to mint tokens when either beforeMint hook is absent or unsuccessful.
+    error ERC721CoreMintingDisabled();
+    
+}
