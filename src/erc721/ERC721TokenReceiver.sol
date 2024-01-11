be9951eead4efab1ba3d2afa469f076eacff5c9b
Krishang <krishang@thirdweb.com>
2024-01-11 18:38:16 +0530

    Delete unused ERC721TokenReceiver

--- src/erc721/ERC721TokenReceiver.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache 2.0
-pragma solidity ^0.8.0;
-
-abstract contract ERC721TokenReceiver {
-    function onERC721Received(address, address, uint256, bytes calldata) external virtual returns (bytes4) {
-        return ERC721TokenReceiver.onERC721Received.selector;
-    }
-}
