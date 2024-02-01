05ca93ed95a1fbf130b45909a0a8520e0e8a94bf
Yash <kumaryashcse@gmail.com>
2024-02-02 04:40:24 +0530

    prettier

--- src/hook/metadata/LazyMintMetadataHook.sol
+++ src/hook/metadata/LazyMintMetadataHook.sol
@@ -7,16 +7,16 @@ import { ERC721Hook } from "../ERC721Hook.sol";
 import { LibString } from "../../lib/LibString.sol";
 
 contract LazyMintMetadataHook is ERC721Hook {
-  using LibString for uint256;
+    using LibString for uint256;
 
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                                CONSTANTS
     //////////////////////////////////////////////////////////////*/
 
-  /// @notice The bits that represent the admin role.
-  uint256 public constant ADMIN_ROLE_BITS = 2 ** 1;
+    /// @notice The bits that represent the admin role.
+    uint256 public constant ADMIN_ROLE_BITS = 2 ** 1;
 
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                                EVENTS
     //////////////////////////////////////////////////////////////*/
 
@@ -29,102 +29,97 @@ contract LazyMintMetadataHook is ERC721Hook {
         bytes encryptedBaseURI
     );
 
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                                ERRORS
     //////////////////////////////////////////////////////////////*/
 
-  /// @notice Emitted when caller is not token core admin.
-  error LazyMintMetadataHookNotAuthorized();
+    /// @notice Emitted when caller is not token core admin.
+    error LazyMintMetadataHookNotAuthorized();
 
-  /// @notice Emitted when querying an invalid index in a batch array.
-  error LazyMintMetadataHookInvalidIndex();
+    /// @notice Emitted when querying an invalid index in a batch array.
+    error LazyMintMetadataHookInvalidIndex();
 
-  /// @notice Emitted when lazy minting zero tokens.
-  error LazyMintMetadataHookZeroAmount();
+    /// @notice Emitted when lazy minting zero tokens.
+    error LazyMintMetadataHookZeroAmount();
 
-  /// @notice Emitted when querying URI for a non-existent invalid token ID.
-  error LazyMintMetadataHookInvalidTokenId();
+    /// @notice Emitted when querying URI for a non-existent invalid token ID.
+    error LazyMintMetadataHookInvalidTokenId();
 
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                                 STORAGE
     //////////////////////////////////////////////////////////////*/
 
-  /// @notice Mapping from token => batch IDs
-  mapping(address => uint256[]) private _batchIds;
+    /// @notice Mapping from token => batch IDs
+    mapping(address => uint256[]) private _batchIds;
 
-  /// @notice Mapping from token => the next token ID to lazy mint.
-  mapping(address => uint256) private _nextTokenIdToLazyMint;
+    /// @notice Mapping from token => the next token ID to lazy mint.
+    mapping(address => uint256) private _nextTokenIdToLazyMint;
 
-  /// @notice Mapping from token => batchId => baseURI
-  mapping(address => mapping(uint256 => string)) private _baseURI;
+    /// @notice Mapping from token => batchId => baseURI
+    mapping(address => mapping(uint256 => string)) private _baseURI;
 
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                                MODIFIER
     //////////////////////////////////////////////////////////////*/
 
-  /// @notice Checks whether the caller is an admin of the given token.
-  modifier onlyAdmin(address _token) {
-    if (!IPermission(_token).hasRole(msg.sender, ADMIN_ROLE_BITS)) {
-      revert LazyMintMetadataHookNotAuthorized();
+    /// @notice Checks whether the caller is an admin of the given token.
+    modifier onlyAdmin(address _token) {
+        if (!IPermission(_token).hasRole(msg.sender, ADMIN_ROLE_BITS)) {
+            revert LazyMintMetadataHookNotAuthorized();
+        }
+        _;
     }
-    _;
-  }
 
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                                 CONSTRUCTOR
   //////////////////////////////////////////////////////////////*/
 
-  constructor(address _admin) ERC721Hook(_admin) {
-    _disableInitializers();
-  }
+    constructor(address _admin) ERC721Hook(_admin) {
+        _disableInitializers();
+    }
 
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                             VIEW FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
-  /// @notice Returns all hook functions implemented by this hook contract.
-  function getHooks() external pure returns (uint256 hooksImplemented) {
-    hooksImplemented = TOKEN_URI_FLAG;
-  }
-
-  /**
-   *  @notice Returns the count of batches of NFTs for a token.
-   *  @param _token The token address.
-   */
-  function getBaseURICount(address _token) public view returns (uint256) {
-    return _batchIds[_token].length;
-  }
-
-  /**
-   *  @notice Returns the URI to fetch token metadata from.
-   *  @dev Meant to be called by the core token contract.
-   *  @param _id The token ID of the NFT.
-   */
-  function tokenURI(
-    uint256 _id
-  ) external view override returns (string memory) {
-    address token = msg.sender;
-    string memory batchUri = _getBaseURI(token, _id);
-
-    return string(abi.encodePacked(batchUri, _id.toString()));
-  }
-
-  /**
-   *  @notice Returns the ID for the batch of tokens at the given index.
-   *  @param _token The token address.
-   *  @param _index The index of the batch.
-   */
-  function getBatchIdAtIndex(
-    address _token,
-    uint256 _index
-  ) public view returns (uint256) {
-    if (_index >= getBaseURICount(_token)) {
-      revert LazyMintMetadataHookInvalidIndex();
+    /// @notice Returns all hook functions implemented by this hook contract.
+    function getHooks() external pure returns (uint256 hooksImplemented) {
+        hooksImplemented = TOKEN_URI_FLAG;
     }
-    return _batchIds[_token][_index];
-  }
 
-  /*//////////////////////////////////////////////////////////////
+    /**
+     *  @notice Returns the count of batches of NFTs for a token.
+     *  @param _token The token address.
+     */
+    function getBaseURICount(address _token) public view returns (uint256) {
+        return _batchIds[_token].length;
+    }
+
+    /**
+     *  @notice Returns the URI to fetch token metadata from.
+     *  @dev Meant to be called by the core token contract.
+     *  @param _id The token ID of the NFT.
+     */
+    function tokenURI(uint256 _id) external view override returns (string memory) {
+        address token = msg.sender;
+        string memory batchUri = _getBaseURI(token, _id);
+
+        return string(abi.encodePacked(batchUri, _id.toString()));
+    }
+
+    /**
+     *  @notice Returns the ID for the batch of tokens at the given index.
+     *  @param _token The token address.
+     *  @param _index The index of the batch.
+     */
+    function getBatchIdAtIndex(address _token, uint256 _index) public view returns (uint256) {
+        if (_index >= getBaseURICount(_token)) {
+            revert LazyMintMetadataHookInvalidIndex();
+        }
+        return _batchIds[_token][_index];
+    }
+
+    /*//////////////////////////////////////////////////////////////
                             EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
@@ -155,58 +150,35 @@ contract LazyMintMetadataHook is ERC721Hook {
         return batchId;
     }
 
-    uint256 startId = _nextTokenIdToLazyMint[_token];
-
-    (_nextTokenIdToLazyMint[_token], batchId) = _batchMintMetadata(
-      _token,
-      startId,
-      _amount,
-      _baseURIForTokens
-    );
-
-    emit TokensLazyMinted(
-      _token,
-      startId,
-      startId + _amount - 1,
-      _baseURIForTokens,
-      _data
-    );
-
-    return batchId;
-  }
-
-  /*//////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
                             INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
-  /// @dev Mints a batch of tokenIds and associates a common baseURI to all those Ids.
-  function _batchMintMetadata(
-    address _token,
-    uint256 _startId,
-    uint256 _amountToMint,
-    string memory _baseURIForTokens
-  ) internal returns (uint256 nextTokenIdToMint, uint256 batchId) {
-    batchId = _startId + _amountToMint;
-    nextTokenIdToMint = batchId;
+    /// @dev Mints a batch of tokenIds and associates a common baseURI to all those Ids.
+    function _batchMintMetadata(
+        address _token,
+        uint256 _startId,
+        uint256 _amountToMint,
+        string memory _baseURIForTokens
+    ) internal returns (uint256 nextTokenIdToMint, uint256 batchId) {
+        batchId = _startId + _amountToMint;
+        nextTokenIdToMint = batchId;
 
-    _batchIds[_token].push(batchId);
+        _batchIds[_token].push(batchId);
 
-    _baseURI[_token][batchId] = _baseURIForTokens;
-  }
-
-  /// @dev Returns the baseURI for a token. The intended metadata URI for the token is baseURI + tokenId.
-  function _getBaseURI(
-    address _token,
-    uint256 _tokenId
-  ) internal view returns (string memory) {
-    uint256 numOfTokenBatches = getBaseURICount(_token);
-    uint256[] memory indices = _batchIds[_token];
-
-    for (uint256 i = 0; i < numOfTokenBatches; i += 1) {
-      if (_tokenId < indices[i]) {
-        return _baseURI[_token][indices[i]];
-      }
+        _baseURI[_token][batchId] = _baseURIForTokens;
     }
-    revert LazyMintMetadataHookInvalidTokenId();
-  }
+
+    /// @dev Returns the baseURI for a token. The intended metadata URI for the token is baseURI + tokenId.
+    function _getBaseURI(address _token, uint256 _tokenId) internal view returns (string memory) {
+        uint256 numOfTokenBatches = getBaseURICount(_token);
+        uint256[] memory indices = _batchIds[_token];
+
+        for (uint256 i = 0; i < numOfTokenBatches; i += 1) {
+            if (_tokenId < indices[i]) {
+                return _baseURI[_token][indices[i]];
+            }
+        }
+        revert LazyMintMetadataHookInvalidTokenId();
+    }
 }
--- test/mocks/MockHookImpl.sol
+++ test/mocks/MockHookImpl.sol
@@ -5,45 +5,37 @@ import "src/hook/ERC721Hook.sol";
 import "src/hook/ERC20Hook.sol";
 
 contract MockOneHookImpl is ERC721Hook {
-  constructor(address _admin) ERC721Hook(_admin) {
-    _disableInitializers();
-  }
+    constructor(address _admin) ERC721Hook(_admin) {
+        _disableInitializers();
+    }
 
-  function getHooks() external pure returns (uint256 hooksImplemented) {
-    hooksImplemented = BEFORE_TRANSFER_FLAG;
-  }
+    function getHooks() external pure returns (uint256 hooksImplemented) {
+        hooksImplemented = BEFORE_TRANSFER_FLAG;
+    }
 }
 
 contract MockFourHookImpl is ERC721Hook {
-  constructor(address _admin) ERC721Hook(_admin) {
-    _disableInitializers();
-  }
+    constructor(address _admin) ERC721Hook(_admin) {
+        _disableInitializers();
+    }
 
-  function getHooks() external pure returns (uint256 hooksImplemented) {
-    hooksImplemented =
-      BEFORE_MINT_FLAG |
-      BEFORE_TRANSFER_FLAG |
-      BEFORE_BURN_FLAG |
-      BEFORE_APPROVE_FLAG;
-  }
+    function getHooks() external pure returns (uint256 hooksImplemented) {
+        hooksImplemented = BEFORE_MINT_FLAG | BEFORE_TRANSFER_FLAG | BEFORE_BURN_FLAG | BEFORE_APPROVE_FLAG;
+    }
 }
 
 contract MockOneHookImpl20 is ERC20Hook {
-  constructor() {}
+    constructor() {}
 
-  function getHooks() external pure returns (uint256 hooksImplemented) {
-    hooksImplemented = BEFORE_TRANSFER_FLAG;
-  }
+    function getHooks() external pure returns (uint256 hooksImplemented) {
+        hooksImplemented = BEFORE_TRANSFER_FLAG;
+    }
 }
 
 contract MockFourHookImpl20 is ERC20Hook {
-  constructor() {}
+    constructor() {}
 
-  function getHooks() external pure returns (uint256 hooksImplemented) {
-    hooksImplemented =
-      BEFORE_MINT_FLAG |
-      BEFORE_TRANSFER_FLAG |
-      BEFORE_BURN_FLAG |
-      BEFORE_APPROVE_FLAG;
-  }
+    function getHooks() external pure returns (uint256 hooksImplemented) {
+        hooksImplemented = BEFORE_MINT_FLAG | BEFORE_TRANSFER_FLAG | BEFORE_BURN_FLAG | BEFORE_APPROVE_FLAG;
+    }
 }
