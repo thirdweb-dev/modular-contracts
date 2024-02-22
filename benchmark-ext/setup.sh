09895abced748d695a6749843477e1cd199dd0ca
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2024-02-22 08:04:08 +0000

    setup with patch

--- benchmark-ext/setup.sh
+++ benchmark-ext/setup.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 DEST=lib/thirdweb-contracts-next/
 
 rm -rf $DEST
 mkdir -p $DEST
 
 cp -a ../lib/ ../src/ ../foundry.toml ../remappings.txt $DEST
+
+cd lib/creator-core-extensions-solidity && git apply ../../patch/manifold_creator_core_patch.diff
