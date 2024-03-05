68138beaeca347e6944e86456d6d22e33961b0cc
Jake Loo <2171134+jakeloo@users.noreply.github.com>
2024-03-05 07:32:00 -0800

    Benchmark script (#53)
    * Remove benchmark-ext in favor of script on a real network
    
    * Benchmark scripts
    
    * Mint, transfer

---
+++ script/benchmark-ext/erc721/BenchmarkERC721.s.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {IERC721} from "./BenchmarkERC721Base.sol";
+import {BenchmarkERC721ThirdwebLegacy} from "./BenchmarkERC721ThirdwebLegacy.sol";
+import {BenchmarkERC721Manifold} from "./BenchmarkERC721Manifold.sol";
+
+contract BenchmarkERC721 is Script {
+    function benchmarkThirdwebLegacy() public {
+        BenchmarkERC721ThirdwebLegacy benchmark = new BenchmarkERC721ThirdwebLegacy();
+
+        address contractAddress = benchmark.deployContract(
+            address(benchmark),
+            "BenchmarkERC721",
+            "B721",
+            "ipfs://QmSkieqXz9voc614Q5LAhQY13LjX6bUzqp2dpDdJhARL7X/0"
+        );
+        benchmark.mintToken(contractAddress);
+        IERC721(contractAddress).setApprovalForAll(address(benchmark), true);
+        benchmark.transferTokenFrom(contractAddress);
+    }
+
+    function benchmarkManifold() public {
+        BenchmarkERC721Manifold benchmark = new BenchmarkERC721Manifold();
+        address contractAddress = benchmark.deployContract(
+            address(0),
+            "BenchmarkERC721",
+            "B721",
+            ""
+        );
+        benchmark.mintToken(contractAddress);
+        IERC721(contractAddress).setApprovalForAll(address(benchmark), true);
+        benchmark.transferTokenFrom(contractAddress);
+    }
+
+    function benchmarkThirdwebNext() public {}
+
+    function benchmarkZora() public {
+        // premint
+        // https://sepolia.explorer.zora.energy/tx/0x1daffd513abf5a8ab1b73bbc9c9dbb1fb576c64069a9a5b6afd2f34ccda5caf6
+    }
+
+    function run() external {
+        vm.startBroadcast();
+        benchmarkThirdwebLegacy();
+        benchmarkManifold();
+    }
+}
---
+++ script/benchmark-ext/erc721/BenchmarkERC721Base.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.24;
+
+interface IERC721 {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) external;
+
+    function setApprovalForAll(address operator, bool _approved) external;
+}
+
+abstract contract BenchmarkERC721Base {
+    // Step 1: Deploy a contract through factory or create
+    // Step 2: Setup the collection with token URI
+    // Step 3: Mint an NFT
+    // Step 4: Mint 100 NFTs
+    // Step 5: Transfer 1 Token
+    function deployContract(
+        address deployerAddress,
+        string memory name,
+        string memory symbol,
+        string memory contractURI
+    ) external virtual returns (address contractAddress);
+
+    function setupToken(address contractAddress) external virtual;
+
+    function mintToken(address contractAddress) external virtual;
+
+    function mintBatchTokens(address contractAddress) external virtual;
+
+    function transferTokenFrom(address contractAddress) external virtual;
+}
