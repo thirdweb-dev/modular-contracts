29e43595989de76123ce6772fa103ef9d1ea7e79
Krishang <krishang@thirdweb.com>
2024-03-07 12:25:35 +0530

    Move HookInstaller to /core

--- src/core/token/ERC20Core.sol
+++ src/core/token/ERC20Core.sol
@@ -11,7 +11,7 @@ import {IERC20Hook} from "../../interface/hook/IERC20Hook.sol";
 import {IERC20HookInstaller} from "../../interface/hook/IERC20HookInstaller.sol";
 import {IInitCall} from "../../interface/common/IInitCall.sol";
 import {ERC20Initializable} from "./ERC20Initializable.sol";
-import {IHook, HookInstaller} from "../../hook/HookInstaller.sol";
+import {IHook, HookInstaller} from "../HookInstaller.sol";
 
 contract ERC20Core is
     Initializable,
