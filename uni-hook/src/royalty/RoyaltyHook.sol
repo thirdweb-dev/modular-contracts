79ca56d02af558986fbf874e298cdd215a923a27
Krishang <krishang@thirdweb.com>
2024-04-08 13:20:23 +0530

    Add Royalty hook

---
+++ uni-hook/src/royalty/RoyaltyHook.sol
@@ -0,0 +1,170 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import {IHook} from "@core-contracts/interface/IHook.sol";
+
+import {HookFlagsDirectory} from "@core-contracts/hook/HookFlagsDirectory.sol";
+import {OnRoyaltyInfoHook} from "@core-contracts/hook/OnRoyaltyInfoHook.sol";
+
+import {Multicallable} from "@solady/utils/Multicallable.sol";
+
+library RoyaltyHookStorage {
+    /// @custom:storage-location erc7201:royalty.hook.storage
+    /// @dev keccak256(abi.encode(uint256(keccak256("royalty.hook.storage")) - 1)) & ~bytes32(uint256(0xff))
+    bytes32 public constant ROYALTY_HOOK_STORAGE_POSITION =
+        0x7ee93e57dcce937c8a9b57c763d236ae026d58f90462880b3a87d31ffacf4800;
+
+    struct Data {
+        /// @notice Mapping from token => default royalty info.
+        mapping(address => RoyaltyHook.RoyaltyInfo) defaultRoyaltyInfo;
+        /// @notice Mapping from token => tokenId => royalty info.
+        mapping(address => mapping(uint256 => RoyaltyHook.RoyaltyInfo)) royaltyInfoForToken;
+    }
+
+    function data() internal pure returns (Data storage data_) {
+        bytes32 position = ROYALTY_HOOK_STORAGE_POSITION;
+        assembly {
+            data_.slot := position
+        }
+    }
+}
+
+contract RoyaltyHook is IHook, HookFlagsDirectory, OnRoyaltyInfoHook, Multicallable {
+    /*//////////////////////////////////////////////////////////////
+                                STRUCTS
+    //////////////////////////////////////////////////////////////*/
+
+    struct RoyaltyInfo {
+        address recipient;
+        uint256 bps;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when the default royalty info for a token is updated.
+    event DefaultRoyaltyUpdate(address indexed token, address indexed recipient, uint256 bps);
+
+    /// @notice Emitted when the royalty info for a specific NFT of a token collection is updated.
+    event TokenRoyaltyUpdate(address indexed token, uint256 indexed tokenId, address indexed recipient, uint256 bps);
+
+    /*//////////////////////////////////////////////////////////////
+                               ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when caller is not token core admin.
+    error RoyaltyHookNotAuthorized();
+
+    /// @notice Emitted when royalty BPS exceeds 10,000.
+    error RoyaltyHookExceedsMaxBps();
+
+    /*//////////////////////////////////////////////////////////////
+                               VIEW FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     *  @notice Returns all hooks implemented by the contract and all hook contract functions to register as
+     *          callable via core contract fallback function.
+     */
+    function getHookInfo() external pure returns (HookInfo memory hookInfo) {
+        hookInfo.hookFlags = ON_ROYALTY_INFO_FLAG;
+        hookInfo.hookFallbackFunctions = new HookFallbackFunction[](4);
+        hookInfo.hookFallbackFunctions[0] =
+            HookFallbackFunction(this.getRoyaltyInfoForToken.selector, CallType.STATICCALL, false);
+        hookInfo.hookFallbackFunctions[1] =
+            HookFallbackFunction(this.getDefaultRoyaltyInfo.selector, CallType.STATICCALL, false);
+        hookInfo.hookFallbackFunctions[2] =
+            HookFallbackFunction(this.setDefaultRoyaltyInfo.selector, CallType.CALL, true);
+        hookInfo.hookFallbackFunctions[3] =
+            HookFallbackFunction(this.setRoyaltyInfoForToken.selector, CallType.CALL, true);
+    }
+
+    /**
+     *  @notice Returns the royalty recipient and amount for a given sale.
+     *  @dev Meant to be called by a token contract.
+     *  @param _tokenId The token ID of the NFT.
+     *  @param _salePrice The sale price of the NFT.
+     *  @return receiver The royalty recipient address.
+     *  @return royaltyAmount The royalty amount to send to the recipient as part of a sale.
+     */
+    function onRoyaltyInfo(uint256 _tokenId, uint256 _salePrice)
+        external
+        view
+        virtual
+        override
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        address token = msg.sender;
+        (address recipient, uint256 bps) = getRoyaltyInfoForToken(token, _tokenId);
+        receiver = recipient;
+        royaltyAmount = (_salePrice * bps) / 10_000;
+    }
+
+    /**
+     *  @notice Returns the overriden royalty info for a given token.
+     *  @param _token The token address.
+     *  @param _tokenId The token ID of the NFT.
+     *  @return recipient The royalty recipient address.
+     *  @return bps The basis points of the sale price that is taken as royalty.
+     */
+    function getRoyaltyInfoForToken(address _token, uint256 _tokenId) public view returns (address, uint16) {
+        RoyaltyHookStorage.Data storage data = RoyaltyHookStorage.data();
+
+        RoyaltyInfo memory royaltyForToken = data.royaltyInfoForToken[_token][_tokenId];
+        RoyaltyInfo memory defaultRoyaltyInfo = data.defaultRoyaltyInfo[_token];
+
+        return royaltyForToken.recipient == address(0)
+            ? (defaultRoyaltyInfo.recipient, uint16(defaultRoyaltyInfo.bps))
+            : (royaltyForToken.recipient, uint16(royaltyForToken.bps));
+    }
+
+    /**
+     *  @notice Returns the default royalty info for a given token.
+     *  @param _token The token address.
+     *  @return recipient The royalty recipient address.
+     *  @return bps The basis points of the sale price that is taken as royalty.
+     */
+    function getDefaultRoyaltyInfo(address _token) external view returns (address, uint16) {
+        RoyaltyInfo memory defaultRoyaltyInfo = RoyaltyHookStorage.data().defaultRoyaltyInfo[_token];
+        return (defaultRoyaltyInfo.recipient, uint16(defaultRoyaltyInfo.bps));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     *  @notice Sets the default royalty info for a given token.
+     *  @param _royaltyRecipient The royalty recipient address.
+     *  @param _royaltyBps The basis points of the sale price that is taken as royalty.
+     */
+    function setDefaultRoyaltyInfo(address _royaltyRecipient, uint256 _royaltyBps) external {
+        address token = msg.sender;
+        if (_royaltyBps > 10_000) {
+            revert RoyaltyHookExceedsMaxBps();
+        }
+
+        RoyaltyHookStorage.data().defaultRoyaltyInfo[token] =
+            RoyaltyInfo({recipient: _royaltyRecipient, bps: _royaltyBps});
+
+        emit DefaultRoyaltyUpdate(token, _royaltyRecipient, _royaltyBps);
+    }
+
+    /**
+     *  @notice Sets the royalty info for a specific NFT of a token collection.
+     *  @param _tokenId The token ID of the NFT.
+     *  @param _recipient The royalty recipient address.
+     *  @param _bps The basis points of the sale price that is taken as royalty.
+     */
+    function setRoyaltyInfoForToken(uint256 _tokenId, address _recipient, uint256 _bps) external {
+        address token = msg.sender;
+        if (_bps > 10_000) {
+            revert RoyaltyHookExceedsMaxBps();
+        }
+
+        RoyaltyHookStorage.data().royaltyInfoForToken[token][_tokenId] = RoyaltyInfo({recipient: _recipient, bps: _bps});
+
+        emit TokenRoyaltyUpdate(token, _tokenId, _recipient, _bps);
+    }
+}
