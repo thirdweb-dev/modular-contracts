627c7575498cbeaa2abdee8068f4cc80909e91e9
nkrishang <62195808+nkrishang@users.noreply.github.com>
2024-04-30 19:18:35 +0530

    Updated extension contracts in /extension (#91)
    * .vscode
    
    * replace /hooks with /extension
    
    * rename files
    
    * reorg files
    
    * Simplify and make uniform: LazyMint.sol -> BatchMetadata.sol
    
    * Comments + simplify language
    
    ---------
    
    Co-authored-by: Jake Loo <2171134+jakeloo@users.noreply.github.com>

---
+++ extension/src/token/Royalty.sol
@@ -0,0 +1,150 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.0;
+
+import {IExtensionContract} from "@core-contracts/interface/IExtensionContract.sol";
+
+library RoyaltyStorage {
+    /// @custom:storage-location erc7201:royalty.storage
+    bytes32 public constant ROYALTY_STORAGE_POSITION =
+        keccak256(abi.encode(uint256(keccak256("royalty.storage")) - 1)) & ~bytes32(uint256(0xff));
+
+    struct Data {
+        /// @notice token => default royalty info
+        mapping(address => Royalty.RoyaltyInfo) defaultRoyaltyInfo;
+        /// @notice token => tokenId => royalty info
+        mapping(address => mapping(uint256 => Royalty.RoyaltyInfo)) royaltyInfoForToken;
+    }
+
+    function data() internal pure returns (Data storage data_) {
+        bytes32 position = ROYALTY_STORAGE_POSITION;
+        assembly {
+            data_.slot := position
+        }
+    }
+}
+
+contract Royalty is IExtensionContract {
+    /*//////////////////////////////////////////////////////////////
+                                STRUCTS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+    *   @notice RoyaltyInfo struct to store royalty information.
+    *   @param recipient The address that will receive the royalty payment.
+    *   @param bps The percentage of a secondary sale that will be paid as royalty.
+    */
+    struct RoyaltyInfo {
+        address recipient;
+        uint256 bps;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when the default royalty info for a token is updated.
+    event DefaultRoyaltyUpdate(address indexed token, address indexed recipient, uint256 bps);
+
+    /// @notice Emitted when the royalty info for a specific NFT is updated.
+    event TokenRoyaltyUpdate(address indexed token, uint256 indexed tokenId, address indexed recipient, uint256 bps);
+
+    /*//////////////////////////////////////////////////////////////
+                               ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Emitted when royalty BPS exceeds 10,000.
+    error RoyaltyExceedsMaxBps();
+
+    /*//////////////////////////////////////////////////////////////
+                               EXTENSION CONFIG
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Returns all implemented callback and extension functions.
+    function getExtensionConfig() external pure returns (ExtensionConfig memory config) {
+        config.callbackFunctions = new bytes4[](0);
+        config.extensionABI = new ExtensionFunction[](5);
+
+        config.extensionABI[0] =
+            ExtensionFunction({selector: this.royaltyInfo.selector, callType: CallType.STATICCALL, permissioned: false});
+        config.extensionABI[1] = ExtensionFunction({
+            selector: this.getDefaultRoyaltyInfo.selector,
+            callType: CallType.STATICCALL,
+            permissioned: false
+        });
+        config.extensionABI[2] = ExtensionFunction({
+            selector: this.getRoyaltyInfoForToken.selector,
+            callType: CallType.STATICCALL,
+            permissioned: false
+        });
+        config.extensionABI[3] = ExtensionFunction({
+            selector: this.setDefaultRoyaltyInfo.selector,
+            callType: CallType.CALL,
+            permissioned: true
+        });
+        config.extensionABI[4] = ExtensionFunction({
+            selector: this.setRoyaltyInfoForToken.selector,
+            callType: CallType.CALL,
+            permissioned: true
+        });
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            EXTENSION FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Returns the royalty recipient and amount for a given sale.
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice)
+        external
+        view
+        virtual
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        address token = msg.sender;
+
+        (address overrideRecipient, uint16 overrideBps) = getRoyaltyInfoForToken(token, _tokenId);
+        (address defaultRecipient, uint16 defaultBps) = getDefaultRoyaltyInfo(token);
+
+        receiver = overrideRecipient == address(0) ? defaultRecipient : overrideRecipient;
+
+        uint16 bps = overrideBps == 0 ? defaultBps : overrideBps;
+        royaltyAmount = (_salePrice * bps) / 10_000;
+    }
+
+    /// @notice Returns the overriden royalty info for a given token.
+    function getRoyaltyInfoForToken(address _token, uint256 _tokenId) public view returns (address, uint16) {
+        RoyaltyStorage.Data storage data = RoyaltyStorage.data();
+        RoyaltyInfo memory royaltyForToken = data.royaltyInfoForToken[_token][_tokenId];
+
+        return (royaltyForToken.recipient, uint16(royaltyForToken.bps));
+    }
+
+    /// @notice Returns the default royalty info for a given token.
+    function getDefaultRoyaltyInfo(address _token) public view returns (address, uint16) {
+        RoyaltyInfo memory defaultRoyaltyInfo = RoyaltyStorage.data().defaultRoyaltyInfo[_token];
+        return (defaultRoyaltyInfo.recipient, uint16(defaultRoyaltyInfo.bps));
+    }
+
+    /// @notice Sets the default royalty info for a given token.
+    function setDefaultRoyaltyInfo(address _royaltyRecipient, uint256 _royaltyBps) external {
+        address token = msg.sender;
+        if (_royaltyBps > 10_000) {
+            revert RoyaltyExceedsMaxBps();
+        }
+
+        RoyaltyStorage.data().defaultRoyaltyInfo[token] = RoyaltyInfo({recipient: _royaltyRecipient, bps: _royaltyBps});
+
+        emit DefaultRoyaltyUpdate(token, _royaltyRecipient, _royaltyBps);
+    }
+
+    /// @notice Sets the royalty info for a specific NFT of a token collection.
+    function setRoyaltyInfoForToken(uint256 _tokenId, address _recipient, uint256 _bps) external {
+        address token = msg.sender;
+        if (_bps > 10_000) {
+            revert RoyaltyExceedsMaxBps();
+        }
+
+        RoyaltyStorage.data().royaltyInfoForToken[token][_tokenId] = RoyaltyInfo({recipient: _recipient, bps: _bps});
+
+        emit TokenRoyaltyUpdate(token, _tokenId, _recipient, _bps);
+    }
+}
--- extension/src/token/SignatureMint.sol
+++ extension/src/token/SignatureMint.sol
@@ -1,47 +1,31 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity ^0.8.0;
 
-import {IHook} from "@core-contracts/interface/IHook.sol";
-
-import {HookFlagsDirectory} from "@core-contracts/hook/HookFlagsDirectory.sol";
-import {BeforeMintHookERC20} from "@core-contracts/hook/BeforeMintHookERC20.sol";
-import {BeforeMintHookERC721} from "@core-contracts/hook/BeforeMintHookERC721.sol";
-import {BeforeMintHookERC1155} from "@core-contracts/hook/BeforeMintHookERC1155.sol";
-
+import {IExtensionContract} from "@core-contracts/interface/IExtensionContract.sol";
 import {Ownable} from "@solady/auth/Ownable.sol";
 import {ECDSA} from "@solady/utils/ECDSA.sol";
 import {EIP712} from "@solady/utils/EIP712.sol";
-import {Multicallable} from "@solady/utils/Multicallable.sol";
 import {SafeTransferLib} from "@solady/utils/SafeTransferLib.sol";
 
-library SignatureMintHookStorage {
-    /// @custom:storage-location erc7201:mint.hook.storage
-    /// @dev keccak256(abi.encode(uint256(keccak256("signature.mint.hook.storage")) - 1)) & ~bytes32(uint256(0xff))
-    bytes32 public constant SIGNATURE_MINT_HOOK_STORAGE_POSITION =
-        keccak256(abi.encode(uint256(keccak256("signature.mint.hook.storage")) - 1)) & ~bytes32(uint256(0xff));
+library SignatureMintStorage {
+    /// @custom:storage-location erc7201:signature.mint.storage
+    bytes32 public constant SIGNATURE_MINT_STORAGE_POSITION =
+        keccak256(abi.encode(uint256(keccak256("signature.mint.storage")) - 1)) & ~bytes32(uint256(0xff));
 
     struct Data {
         mapping(bytes32 => bool) uidUsed;
-        mapping(address => SignatureMintHook.SaleConfig) saleConfig;
+        mapping(address => SignatureMint.SaleConfig) saleConfig;
     }
 
     function data() internal pure returns (Data storage data_) {
-        bytes32 position = SIGNATURE_MINT_HOOK_STORAGE_POSITION;
+        bytes32 position = SIGNATURE_MINT_STORAGE_POSITION;
         assembly {
             data_.slot := position
         }
     }
 }
 
-contract SignatureMintHook is
-    IHook,
-    HookFlagsDirectory,
-    BeforeMintHookERC20,
-    BeforeMintHookERC721,
-    BeforeMintHookERC1155,
-    EIP712,
-    Multicallable
-{
+contract SignatureMint is IExtensionContract, EIP712 {
     using ECDSA for bytes32;
 
     /*//////////////////////////////////////////////////////////////
@@ -107,13 +91,12 @@ contract SignatureMintHook is
                                 ERRORS
     //////////////////////////////////////////////////////////////*/
 
-    error MintHookUnrecognizedParams();
-    error MintHookIncorrectNativeTokenSent();
-    error MintHookRequestExpired();
-    error MintHookRequestUidReused();
-    error MintHookRequestInvalidToken();
-    error MintHookRequestMismatch();
-    error MintHookRequestUnauthorizedSignature();
+    error SignatureMintIncorrectNativeTokenSent();
+    error SigantureMintRequestExpired();
+    error SignatureMintRequestUidReused();
+    error SignatureMintRequestInvalidToken();
+    error SignatureMintRequestMismatch();
+    error SignatureMintRequestUnauthorizedSignature();
 
     /*//////////////////////////////////////////////////////////////
                                 CONSTANTS
@@ -134,21 +117,34 @@ contract SignatureMintHook is
     address private constant NATIVE_TOKEN_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     /*//////////////////////////////////////////////////////////////
-                            HOOK FUNCTIONS
+                            EXTENSION CONFIG
     //////////////////////////////////////////////////////////////*/
 
-    function getHookInfo() external pure returns (HookInfo memory info) {
-        info.hookFlags = BEFORE_MINT_ERC20_FLAG | BEFORE_MINT_ERC721_FLAG | BEFORE_MINT_ERC1155_FLAG;
-        info.hookFallbackFunctions = new HookFallbackFunction[](2);
-        info.hookFallbackFunctions[0] = HookFallbackFunction(this.getSaleConfig.selector, CallType.STATICCALL, false);
-        info.hookFallbackFunctions[1] = HookFallbackFunction(this.setSaleConfig.selector, CallType.CALL, true);
+    function getExtensionConfig() external pure returns (ExtensionConfig memory config) {
+        config.callbackFunctions = new bytes4[](3);
+        config.extensionABI = new ExtensionFunction[](2);
+
+        config.callbackFunctions[0] = this.beforeMintERC20.selector;
+        config.callbackFunctions[1] = this.beforeMintERC721.selector;
+        config.callbackFunctions[2] = this.beforeMintERC1155.selector;
+
+        config.extensionABI[0] = ExtensionFunction({
+            selector: this.getSaleConfig.selector,
+            callType: CallType.STATICCALL,
+            permissioned: false
+        });
+        config.extensionABI[1] =
+            ExtensionFunction({selector: this.setSaleConfig.selector, callType: CallType.CALL, permissioned: false});
     }
+
+    /*//////////////////////////////////////////////////////////////
+                            CALLBACK FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
 
     function beforeMintERC20(address _to, uint256 _quantity, bytes memory _data)
         external
         payable
         virtual
-        override
         returns (bytes memory)
     {
         SignatureMintParamsERC20 memory _params = abi.decode(_data, (SignatureMintParamsERC20));
@@ -159,7 +155,6 @@ contract SignatureMintHook is
         external
         payable
         virtual
-        override
         returns (bytes memory)
     {
         SignatureMintParamsERC721 memory _params = abi.decode(_data, (SignatureMintParamsERC721));
@@ -170,7 +165,6 @@ contract SignatureMintHook is
         external
         payable
         virtual
-        override
         returns (bytes memory)
     {
         SignatureMintParamsERC1155 memory _params = abi.decode(_data, (SignatureMintParamsERC1155));
@@ -178,23 +172,7 @@ contract SignatureMintHook is
     }
 
     /*//////////////////////////////////////////////////////////////
-                            ENCODE FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
-    function signatureMintERC20(SignatureMintParamsERC20 memory _params) external pure returns (bytes memory) {
-        return abi.encode(_params);
-    }
-
-    function signatureMintERC721(SignatureMintParamsERC721 memory _params) external pure returns (bytes memory) {
-        return abi.encode(_params);
-    }
-
-    function signatureMintERC1155(SignatureMintParamsERC1155 memory _params) external pure returns (bytes memory) {
-        return abi.encode(_params);
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                            HOOK FALLBACK FUNCTIONS
+                            EXTENSION FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
     function getSaleConfig(address _token)
@@ -202,7 +180,7 @@ contract SignatureMintHook is
         view
         returns (address primarySaleRecipient, address platformFeeRecipient, uint16 platformFeeBps)
     {
-        SaleConfig memory saleConfig = _signatureMintHookStorage().saleConfig[_token];
+        SaleConfig memory saleConfig = _signatureMintStorage().saleConfig[_token];
         return (saleConfig.primarySaleRecipient, saleConfig.platformFeeRecipient, saleConfig.platformFeeBps);
     }
 
@@ -210,7 +188,7 @@ contract SignatureMintHook is
         external
     {
         address token = msg.sender;
-        _signatureMintHookStorage().saleConfig[token] =
+        _signatureMintStorage().saleConfig[token] =
             SaleConfig(_primarySaleRecipient, _platformFeeRecipient, _platformFeeBps);
     }
 
@@ -225,19 +203,19 @@ contract SignatureMintHook is
         bytes memory _signature
     ) internal {
         if (_req.token != msg.sender) {
-            revert MintHookRequestInvalidToken();
+            revert SignatureMintRequestInvalidToken();
         }
 
         if (_req.recipient != _expectedRecipient || _req.quantity != _expectedAmount) {
-            revert MintHookRequestMismatch();
+            revert SignatureMintRequestMismatch();
         }
 
         if (block.timestamp < _req.startTimestamp || _req.endTimestamp <= block.timestamp) {
-            revert MintHookRequestExpired();
+            revert SigantureMintRequestExpired();
         }
 
-        if (_signatureMintHookStorage().uidUsed[_req.uid]) {
-            revert MintHookRequestUidReused();
+        if (_signatureMintStorage().uidUsed[_req.uid]) {
+            revert SignatureMintRequestUidReused();
         }
 
         address signer = _hashTypedData(
@@ -257,10 +235,10 @@ contract SignatureMintHook is
         ).recover(_signature);
 
         if (Ownable(_req.token).owner() != signer) {
-            revert MintHookRequestUnauthorizedSignature();
+            revert SignatureMintRequestUnauthorizedSignature();
         }
 
-        _signatureMintHookStorage().uidUsed[_req.uid] = true;
+        _signatureMintStorage().uidUsed[_req.uid] = true;
 
         _distributeMintPrice(_req.recipient, _req.currency, (_req.quantity * _req.pricePerUnit) / 1e18);
     }
@@ -272,19 +250,19 @@ contract SignatureMintHook is
         bytes memory _signature
     ) internal {
         if (_req.token != msg.sender) {
-            revert MintHookRequestInvalidToken();
+            revert SignatureMintRequestInvalidToken();
         }
 
         if (_req.recipient != _expectedRecipient || _req.quantity != _expectedAmount) {
-            revert MintHookRequestMismatch();
+            revert SignatureMintRequestMismatch();
         }
 
         if (block.timestamp < _req.startTimestamp || _req.endTimestamp <= block.timestamp) {
-            revert MintHookRequestExpired();
+            revert SigantureMintRequestExpired();
         }
 
-        if (_signatureMintHookStorage().uidUsed[_req.uid]) {
-            revert MintHookRequestUidReused();
+        if (_signatureMintStorage().uidUsed[_req.uid]) {
+            revert SignatureMintRequestUidReused();
         }
 
         address signer = _hashTypedData(
@@ -304,10 +282,10 @@ contract SignatureMintHook is
         ).recover(_signature);
 
         if (Ownable(_req.token).owner() != signer) {
-            revert MintHookRequestUnauthorizedSignature();
+            revert SignatureMintRequestUnauthorizedSignature();
         }
 
-        _signatureMintHookStorage().uidUsed[_req.uid] = true;
+        _signatureMintStorage().uidUsed[_req.uid] = true;
 
         _distributeMintPrice(_req.recipient, _req.currency, _req.quantity * _req.pricePerUnit);
     }
@@ -320,21 +298,21 @@ contract SignatureMintHook is
         bytes memory _signature
     ) internal {
         if (_req.token != msg.sender) {
-            revert MintHookRequestInvalidToken();
+            revert SignatureMintRequestInvalidToken();
         }
 
         if (
             _req.recipient != _expectedRecipient || _req.quantity != _expectedAmount || _req.tokenId != _expectedTokenId
         ) {
-            revert MintHookRequestMismatch();
+            revert SignatureMintRequestMismatch();
         }
 
         if (block.timestamp < _req.startTimestamp || _req.endTimestamp <= block.timestamp) {
-            revert MintHookRequestExpired();
+            revert SigantureMintRequestExpired();
         }
 
-        if (_signatureMintHookStorage().uidUsed[_req.uid]) {
-            revert MintHookRequestUidReused();
+        if (_signatureMintStorage().uidUsed[_req.uid]) {
+            revert SignatureMintRequestUidReused();
         }
 
         address signer = _hashTypedData(
@@ -355,10 +333,10 @@ contract SignatureMintHook is
         ).recover(_signature);
 
         if (Ownable(_req.token).owner() != signer) {
-            revert MintHookRequestUnauthorizedSignature();
+            revert SignatureMintRequestUnauthorizedSignature();
         }
 
-        _signatureMintHookStorage().uidUsed[_req.uid] = true;
+        _signatureMintStorage().uidUsed[_req.uid] = true;
 
         _distributeMintPrice(_req.recipient, _req.currency, _req.quantity * _req.pricePerUnit);
     }
@@ -366,18 +344,18 @@ contract SignatureMintHook is
     function _distributeMintPrice(address _owner, address _currency, uint256 _price) internal {
         if (_price == 0) {
             if (msg.value > 0) {
-                revert MintHookIncorrectNativeTokenSent();
+                revert SignatureMintIncorrectNativeTokenSent();
             }
             return;
         }
 
-        SaleConfig memory saleConfig = _signatureMintHookStorage().saleConfig[msg.sender];
+        SaleConfig memory saleConfig = _signatureMintStorage().saleConfig[msg.sender];
 
         uint256 platformFee = (_price * saleConfig.platformFeeBps) / 10_000;
 
         if (_currency == NATIVE_TOKEN_ADDRESS) {
             if (msg.value != _price) {
-                revert MintHookIncorrectNativeTokenSent();
+                revert SignatureMintIncorrectNativeTokenSent();
             }
             SafeTransferLib.safeTransferETH(saleConfig.primarySaleRecipient, _price - platformFee);
             SafeTransferLib.safeTransferETH(saleConfig.platformFeeRecipient, platformFee);
@@ -392,7 +370,7 @@ contract SignatureMintHook is
         version = "1";
     }
 
-    function _signatureMintHookStorage() internal pure returns (SignatureMintHookStorage.Data storage) {
-        return SignatureMintHookStorage.data();
+    function _signatureMintStorage() internal pure returns (SignatureMintStorage.Data storage) {
+        return SignatureMintStorage.data();
     }
 }
