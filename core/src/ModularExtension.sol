08ed9f8827ff4437d4394369f19f4497affdc3db
Krishang <krishang@thirdweb.com>
2024-05-09 10:52:19 +0530

    Add Extension name and version

--- core/src/ModularExtension.sol
+++ core/src/ModularExtension.sol
@@ -4,5 +4,9 @@ pragma solidity ^0.8.23;
 import {IModularExtension} from "./interface/IModularExtension.sol";
 
 abstract contract ModularExtension is IModularExtension {
+    function extensionName() external pure virtual returns (string memory);
+
+    function extensionVersion() external pure virtual returns (uint256);
+
     function getExtensionConfig() external pure virtual returns (ExtensionConfig memory);
 }
