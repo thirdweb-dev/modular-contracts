935cb7e6fb9e07ec386e05dfd52b9841f484ec0f
Krishang <krishang@thirdweb.com>
2024-05-09 11:00:05 +0530

    Rename extensionName -> extensionID

--- core/src/ModularExtension.sol
+++ core/src/ModularExtension.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.23;
 import {IModularExtension} from "./interface/IModularExtension.sol";
 
 abstract contract ModularExtension is IModularExtension {
-    function extensionName() external pure virtual returns (string memory);
+    function extensionID() external pure virtual returns (string memory);
 
     function extensionVersion() external pure virtual returns (uint256);
 
