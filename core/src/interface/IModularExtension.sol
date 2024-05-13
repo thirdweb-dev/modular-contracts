85879417b046eb60f84b15e5d733880160002e17
Krishang <krishang@thirdweb.com>
2024-05-13 14:07:06 +0530

    Update interface comments

--- core/src/interface/IModularExtension.sol
+++ core/src/interface/IModularExtension.sol
@@ -4,12 +4,8 @@ pragma solidity ^0.8.23;
 import {IExtensionConfig} from "./IExtensionConfig.sol";
 
 interface IModularExtension is IExtensionConfig {
-    /*//////////////////////////////////////////////////////////////
-                            VIEW FUNCTIONS
-    //////////////////////////////////////////////////////////////*/
-
     /**
-     *  @notice Returns all extension functions and supported callback functions of an extension contract.
+     *  @dev Returns the ExtensionConfig of the Extension contract.
      */
     function getExtensionConfig() external pure returns (ExtensionConfig memory);
 }
