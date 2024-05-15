aa30a0dde72fa2322941f4f97c89ff3d0dd564d2
Krishang <krishang@thirdweb.com>
2024-05-15 18:52:39 +0530

    Add comments to IInstallationCallback

--- core/src/interface/IInstallationCallback.sol
+++ core/src/interface/IInstallationCallback.sol
@@ -2,7 +2,19 @@
 pragma solidity ^0.8.23;
 
 interface IInstallationCallback {
+    /**
+     *  @dev Called by a Core into an Extension during the installation of the Extension.
+     *
+     *  @param sender The address of the caller installing the Extension.
+     *  @param data The data passed to the Core's installExtension function.
+     */
     function onInstall(address sender, bytes calldata data) external;
 
+    /**
+     *  @dev Called by a Core into an Extension during the uninstallation of the Extension.
+     *
+     *  @param sender The address of the caller uninstalling the Extension.
+     *  @param data The data passed to the Core's uninstallExtension function.
+     */
     function onUninstall(address sender, bytes calldata data) external;
 }
