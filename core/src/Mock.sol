4339fd6215f19dad4a326ffd2981cd32ee200dcd
Yash <kumaryashcse@gmail.com>
2024-05-16 18:51:37 +0530

    testjam utils

--- core/src/Mock.sol
+++ core/src/Mock.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: Apache 2.0
 pragma solidity ^0.8.0;
 
-// Test utils
-import {Test} from "forge-std/Test.sol";
-import {ERC1967FactoryConstants} from "@solady/utils/ERC1967FactoryConstants.sol";
-
 // Target contract
-import {IExtensionConfig} from "src/interface/IExtensionConfig.sol";
-import {ModularExtension} from "src/ModularExtension.sol";
-import {ModularCoreUpgradeable} from "src/ModularCoreUpgradeable.sol";
+import {IExtensionConfig} from "./interface/IExtensionConfig.sol";
+import {ModularExtension} from "./ModularExtension.sol";
+import {ModularCoreUpgradeable} from "./ModularCoreUpgradeable.sol";
 
-contract MockBase {
+contract DemoBase {
     uint256 internal constant NUMBER_OF_CALLBACK = 10;
 
     function getCallbacks() internal pure virtual returns (IExtensionConfig.CallbackFunction[] memory functions) {
@@ -22,7 +18,7 @@ contract MockBase {
     }
 }
 
-contract MockCore is MockBase, ModularCoreUpgradeable {
+contract DemoCore is DemoBase, ModularCoreUpgradeable {
     constructor(address _erc1967FactoryAddress, address _owner) ModularCoreUpgradeable(_erc1967FactoryAddress) {
         _initializeOwner(_owner);
     }
@@ -51,7 +47,7 @@ contract MockCore is MockBase, ModularCoreUpgradeable {
     }
 }
 
-contract MockExtensionWithFunctions is MockBase, ModularExtension {
+contract DemoExtensionWithFunctions is DemoBase, ModularExtension {
     event CallbackFunctionOne();
     event FallbackFunctionCalled();
 
@@ -147,7 +143,7 @@ contract MockExtensionWithFunctions is MockBase, ModularExtension {
     function permissioned_staticcall() external view {}
 }
 
-contract MockExtensionAlternate is MockExtensionWithFunctions {
+contract DemoExtensionAlternate is DemoExtensionWithFunctions {
     function getExtensionConfig() external pure virtual override returns (ExtensionConfig memory config) {
         config.callbackFunctions = getCallbacks();
 
@@ -207,7 +203,7 @@ contract MockExtensionAlternate is MockExtensionWithFunctions {
     }
 }
 
-contract MockExtensionWithInterfaceId is MockBase, ModularExtension {
+contract DemoExtensionWithInterfaceId is DemoBase, ModularExtension {
     event CallbackFunctionOne();
     event FallbackFunctionCalled();
 
@@ -302,127 +298,4 @@ contract MockExtensionWithInterfaceId is MockBase, ModularExtension {
     function permissioned_delegatecall() external {}
 
     function permissioned_staticcall() external view {}
-}
-
-contract ModularCoreUpgradeableTest is Test {
-    MockCore public core;
-
-    MockExtensionWithFunctions public extensionImplementation;
-    MockExtensionWithInterfaceId public extensionWithInterfaceId;
-    MockExtensionAlternate public newExtensionImplementation;
-
-    address public owner = address(0x1);
-    address public permissionedActor = address(0x2);
-    address public unpermissionedActor = address(0x3);
-
-    function setUp() public {
-        // Deterministic, canonical ERC1967Factory contract
-        vm.etch(ERC1967FactoryConstants.ADDRESS, ERC1967FactoryConstants.BYTECODE);
-
-        core = new MockCore(ERC1967FactoryConstants.ADDRESS, owner);
-
-        extensionImplementation = new MockExtensionWithFunctions();
-        newExtensionImplementation = new MockExtensionAlternate();
-        extensionWithInterfaceId = new MockExtensionWithInterfaceId();
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                        1. Install an extension
-    //////////////////////////////////////////////////////////////*/
-
-    event CallbackFunctionOne();
-    event FallbackFunctionCalled();
-
-    function test_installExtension() public {
-        // 1. Install the extension in the core contract by providing an implementation address.
-        vm.prank(owner);
-        core.installExtension(address(extensionImplementation), "");
-
-        // 2. Callback function is now called
-        vm.expectEmit(true, false, false, false);
-        emit CallbackFunctionOne();
-        core.callbackFunctionOne();
-
-        // 3. Extension functions now callable via the core contract fallback
-        vm.expectEmit(true, false, false, false);
-        emit FallbackFunctionCalled();
-        MockExtensionWithFunctions(address(core)).notPermissioned_call();
-    }
-
-    function test_revert_installExtension() public {
-        vm.prank(owner);
-        vm.expectRevert(abi.encodeWithSelector(ModularCoreUpgradeable.ExtensionInterfaceNotCompatible.selector, 0x1212121200000000000000000000000000000000000000000000000000000000));
-        core.installExtension(address(extensionWithInterfaceId), "");
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                        2. Update extension
-    //////////////////////////////////////////////////////////////*/
-
-    event SomeNewEvent();
-
-    function test_updateExtension() public {
-        // Setup: Install extension in the core contract and set some storage
-        vm.prank(owner);
-        core.installExtension(address(extensionImplementation), "");
-
-        vm.prank(owner);
-        MockExtensionWithFunctions(address(core)).setNumber(42);
-        assertEq(MockExtensionWithFunctions(address(core)).getNumber(), 42);
-
-        // 1. The extension update is going to include an additional extension function in
-        //    the extension config called `someNewFunction`.
-        //
-        //    This function is unavailable prior to the update, but crucial for the extension
-        //    to work according to spec.
-
-        vm.expectRevert(abi.encodeWithSelector(ModularCoreUpgradeable.FallbackFunctionNotInstalled.selector));
-        MockExtensionAlternate(address(core)).someNewFunction();
-
-        // 2. Core contract owner updates the extension used by the core contract by updating
-        //    its implementation via the `updateExtension` API.
-
-        vm.prank(owner);
-        core.updateExtension(address(extensionImplementation), address(newExtensionImplementation));
-
-        // 3. The new extension function is now callable.
-
-        vm.expectEmit(true, false, false, false);
-        emit SomeNewEvent();
-        MockExtensionAlternate(address(core)).someNewFunction();
-
-        // 4. Storage is not lost during the update because the proxy contract is the same, only its implementation is now different.
-        assertEq(MockExtensionAlternate(address(core)).getNumber(), 42);
-    }
-
-    /*//////////////////////////////////////////////////////////////
-                        3. Uninstall extension
-    //////////////////////////////////////////////////////////////*/
-
-    function test_uninstallExtension() public {
-        // Setup: Install extension in the core contract and set some storage
-        vm.prank(owner);
-        core.installExtension(address(extensionImplementation), "");
-
-        // Setup: Update extension implementation to include new function.
-        vm.prank(owner);
-        core.updateExtension(address(extensionImplementation), address(newExtensionImplementation));
-
-        vm.expectEmit(true, false, false, false);
-        emit CallbackFunctionOne();
-        core.callbackFunctionOne();
-
-        // 1. Uninstall the extension from the core contract by providing the current implementation address.
-
-        vm.expectRevert(abi.encodeWithSelector(ModularCoreUpgradeable.ExtensionNotInstalled.selector));
-        vm.prank(owner);
-        core.uninstallExtension(address(extensionImplementation), "");
-
-        vm.prank(owner);
-        core.uninstallExtension(address(newExtensionImplementation), "");
-
-        // 2. E.g. required callback function no longer has a call destination
-        vm.expectRevert(abi.encodeWithSelector(ModularCoreUpgradeable.CallbackFunctionRequired.selector));
-        core.callbackFunctionOne();
-    }
 }
---
+++ core/src/ProxyFactory.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity ^0.8.0;
+
+import {ERC1967Factory} from "@solady/utils/ERC1967Factory.sol";
+
+contract ProxyFactory is ERC1967Factory {}
