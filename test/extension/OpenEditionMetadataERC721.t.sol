9f048d47b40a963fba68506f111ae896e48a08d2
Yash <kumaryashcse@gmail.com>
2024-05-23 21:51:29 +0530

    test OpenEditionMetadataERC721

---
+++ test/extension/OpenEditionMetadataERC721.t.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "lib/forge-std/src/console.sol";
+
+import {Test} from "forge-std/Test.sol";
+import {ERC1967Factory} from "@solady/utils/ERC1967Factory.sol";
+import {ERC1967FactoryConstants} from "@solady/utils/ERC1967FactoryConstants.sol";
+
+// Target contract
+import {IExtensionConfig} from "src/interface/IExtensionConfig.sol";
+import {IModularCore} from "src/interface/IModularCore.sol";
+import {ModularExtension} from "src/ModularExtension.sol";
+import {ModularCoreUpgradeable} from "src/ModularCoreUpgradeable.sol";
+import {ERC721Core} from "src/core/token/ERC721Core.sol";
+import {
+    OpenEditionMetadataERC721,
+    OpenEditionMetadataStorage
+} from "src/extension/token/metadata/OpenEditionMetadataERC721.sol";
+
+contract OpenEditionMetadataExt is OpenEditionMetadataERC721 {
+    function sharedMetadata(address token) external view returns (SharedMetadata memory) {
+        return OpenEditionMetadataStorage.data().sharedMetadata[token];
+    }
+
+    function createMetadataEdition(
+        string memory name,
+        string memory description,
+        string memory imageURI,
+        string memory animationURI,
+        uint256 tokenOfEdition
+    ) external pure returns (string memory) {
+        return _createMetadataEdition(name, description, imageURI, animationURI, tokenOfEdition);
+    }
+}
+
+contract OpenEditionMetadataERC721Test is Test {
+    ERC721Core public core;
+
+    OpenEditionMetadataExt public extensionImplementation;
+    OpenEditionMetadataExt public installedExtension;
+
+    address public owner = address(0x1);
+    address public permissionedActor = address(0x2);
+    address public unpermissionedActor = address(0x3);
+
+    function setUp() public {
+        // Deterministic, canonical ERC1967Factory contract
+        vm.etch(ERC1967FactoryConstants.ADDRESS, ERC1967FactoryConstants.BYTECODE);
+
+        address[] memory extensions;
+        bytes[] memory extensionData;
+
+        core = new ERC721Core(ERC1967FactoryConstants.ADDRESS, "test", "TEST", "", owner, extensions, extensionData);
+        extensionImplementation = new OpenEditionMetadataExt();
+
+        // install extension
+        vm.prank(owner);
+        core.installExtension(address(extensionImplementation), "");
+
+        IModularCore.InstalledExtension[] memory installedExtensions = core.getInstalledExtensions();
+        installedExtension = OpenEditionMetadataExt(installedExtensions[0].implementation);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Unit tests: `setSharedMetadata`
+    //////////////////////////////////////////////////////////////*/
+
+    function test_state_setSharedMetadata() public {
+        OpenEditionMetadataERC721.SharedMetadata memory sharedMetadata = OpenEditionMetadataERC721.SharedMetadata({
+            name: "Test",
+            description: "Test",
+            imageURI: "https://test.com",
+            animationURI: "https://test.com"
+        });
+
+        vm.prank(owner);
+        OpenEditionMetadataExt(address(core)).setSharedMetadata(sharedMetadata);
+
+        // read state from core
+        assertEq(
+            core.tokenURI(1),
+            installedExtension.createMetadataEdition(
+                sharedMetadata.name, sharedMetadata.description, sharedMetadata.imageURI, sharedMetadata.animationURI, 1
+            )
+        );
+    }
+
+    function test_revert_setSharedMetadata() public {
+        OpenEditionMetadataERC721.SharedMetadata memory sharedMetadata;
+
+        vm.expectRevert(0x82b42900); // `Unauthorized()`
+        OpenEditionMetadataExt(address(core)).setSharedMetadata(sharedMetadata);
+    }
+}
