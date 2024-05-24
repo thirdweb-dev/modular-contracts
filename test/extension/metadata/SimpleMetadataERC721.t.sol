cc0881b68ba485d272814d4fec70114c26f6600b
Yash <72552910+kumaryash90@users.noreply.github.com>
2024-05-24 19:34:46 +0530

    Metadata extension tests (#104)
    * test SimpleMetadataERC721
    
    * test SimpleMetadataERC1155
    
    * reorganize tests

---
+++ test/extension/metadata/SimpleMetadataERC721.t.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "lib/forge-std/src/console.sol";
+
+import {Test} from "forge-std/Test.sol";
+import {ERC1967Factory} from "@solady/utils/ERC1967Factory.sol";
+import {ERC1967FactoryConstants} from "@solady/utils/ERC1967FactoryConstants.sol";
+
+// Target contract
+import {IExtensionConfig} from "src/interface/IExtensionConfig.sol";
+import {IModularCore} from "src/interface/IModularCore.sol";
+import {ModularExtension} from "src/ModularExtension.sol";
+import {ModularCoreUpgradeable} from "src/ModularCoreUpgradeable.sol";
+import {ERC721Core} from "src/core/token/ERC721Core.sol";
+import {SimpleMetadataERC721, SimpleMetadataStorage} from "src/extension/token/metadata/SimpleMetadataERC721.sol";
+
+contract SimpleMetadataExt is SimpleMetadataERC721 {
+    function uris(address token, uint256 id) external view returns (string memory) {
+        return SimpleMetadataStorage.data().uris[token][id];
+    }
+}
+
+contract SimpleMetadataERC721Test is Test {
+    ERC721Core public core;
+
+    SimpleMetadataExt public extensionImplementation;
+    SimpleMetadataExt public installedExtension;
+
+    address public owner = address(0x1);
+    address public permissionedActor = address(0x2);
+    address public unpermissionedActor = address(0x3);
+
+    function setUp() public {
+        // Deterministic, canonical ERC1967Factory contract
+        vm.etch(ERC1967FactoryConstants.ADDRESS, ERC1967FactoryConstants.BYTECODE);
+
+        address[] memory extensions;
+        bytes[] memory extensionData;
+
+        core = new ERC721Core(ERC1967FactoryConstants.ADDRESS, "test", "TEST", "", owner, extensions, extensionData);
+        extensionImplementation = new SimpleMetadataExt();
+
+        // install extension
+        vm.prank(owner);
+        core.installExtension(address(extensionImplementation), "");
+
+        IModularCore.InstalledExtension[] memory installedExtensions = core.getInstalledExtensions();
+        installedExtension = SimpleMetadataExt(installedExtensions[0].implementation);
+    }
+
+    /*///////////////////////////////////////////////////////////////
+                        Unit tests: `setTokenURI`
+    //////////////////////////////////////////////////////////////*/
+
+    function test_state_setTokenURI() public {
+        vm.prank(owner);
+        SimpleMetadataExt(address(core)).setTokenURI(1, "ipfs://base/1");
+
+        vm.prank(owner);
+        SimpleMetadataExt(address(core)).setTokenURI(2, "ipfs://base/2");
+
+        // read state from core
+        assertEq(core.tokenURI(1), "ipfs://base/1");
+        assertEq(core.tokenURI(2), "ipfs://base/2");
+        assertEq(core.tokenURI(3), "");
+
+        // read state from the installed extension
+        assertEq(installedExtension.uris(address(core), 1), "ipfs://base/1");
+        assertEq(installedExtension.uris(address(core), 2), "ipfs://base/2");
+        assertEq(installedExtension.uris(address(core), 3), "");
+    }
+
+    function test_revert_setTokenURI() public {
+        vm.expectRevert(0x82b42900); // `Unauthorized()`
+        SimpleMetadataExt(address(core)).setTokenURI(1, "ipfs://base/");
+    }
+}
