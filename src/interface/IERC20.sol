a1f996b0e3cad5e56bb9208ea30afe5ae4d3277d
nkrishang <62195808+nkrishang@users.noreply.github.com>
2024-05-24 22:34:37 +0530

    Add supportsInterface for type(IERC20).interfaceId (#110)
    * Add supportsInterface for type(IERC20).interfaceId
    
    * Add in ERC20CoreInitializable as well

---
+++ src/interface/IERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.23;
+
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
