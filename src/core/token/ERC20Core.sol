18aa611547e654087c17faddfea20e4690422327
Aadam Debevec <aadam.debevec@limitbreak.com>
2024-07-18 11:29:28 -0500

    add creatortoken to erc20core
    Signed-off-by: Aadam Debevec <aadam.debevec@limitbreak.com>

--- src/core/token/ERC20Core.sol
+++ src/core/token/ERC20Core.sol
@@ -7,6 +7,10 @@ import {ERC20} from "@solady/tokens/ERC20.sol";
 import {Multicallable} from "@solady/utils/Multicallable.sol";
 import {ReentrancyGuard} from "@solady/utils/ReentrancyGuard.sol";
 
+import {CreatorToken} from "./CreatorToken/CreatorToken.sol";
+import {TOKEN_TYPE_ERC20} from "@limitbreak/permit-c/Constants.sol";
+import {ITransferValidator} from "@limitbreak/creator-token-standards/interfaces/ITransferValidator.sol";
+
 import {IERC20} from "../../interface/IERC20.sol";
 
 import {BeforeMintCallbackERC20} from "../../callback/BeforeMintCallbackERC20.sol";
@@ -14,7 +18,7 @@ import {BeforeBurnCallbackERC20} from "../../callback/BeforeBurnCallbackERC20.so
 import {BeforeApproveCallbackERC20} from "../../callback/BeforeApproveCallbackERC20.sol";
 import {BeforeTransferCallbackERC20} from "../../callback/BeforeTransferCallbackERC20.sol";
 
-contract ERC20Core is ERC20, Multicallable, ModularCore {
+contract ERC20Core is ERC20, Multicallable, ModularCore, CreatorToken {
     /*//////////////////////////////////////////////////////////////
                                 STORAGE
     //////////////////////////////////////////////////////////////*/
@@ -128,6 +132,10 @@ contract ERC20Core is ERC20, Multicallable, ModularCore {
         _setupContractURI(contractURI);
     }
 
+    function setTransferValidator(address validator) external onlyOwner {
+        _setTransferValidator(validator);
+    }
+
     /**
      *  @notice Mints tokens. Calls the beforeMint hook.
      *  @dev Reverts if beforeMint hook is absent or unsuccessful.
@@ -216,6 +224,10 @@ contract ERC20Core is ERC20, Multicallable, ModularCore {
         emit ContractURIUpdated();
     }
 
+    function _tokenType() internal pure override returns (uint16) {
+        return uint16(TOKEN_TYPE_ERC20);
+    }
+
     /*//////////////////////////////////////////////////////////////
                           CALLBACK INTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
@@ -230,6 +242,10 @@ contract ERC20Core is ERC20, Multicallable, ModularCore {
 
     /// @dev Calls the beforeTransfer hook, if installed.
     function _beforeTransfer(address from, address to, uint256 amount) internal virtual {
+        address transferValidator = getTransferValidator();
+        if (transferValidator != address(0)) {
+            ITransferValidator(transferValidator).validateTransfer(msg.sender, from, to, tokenId);
+        }
         _executeCallbackFunction(
             BeforeTransferCallbackERC20.beforeTransferERC20.selector,
             abi.encodeCall(BeforeTransferCallbackERC20.beforeTransferERC20, (from, to, amount))
