03da7c26773538698472b3006c799b51a8b0f00d
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-09-04 12:28:07 -0400

    Implement tokenIdERC1155 module to handle tokenId management (#147)
    * initial commit
    
    * created tests for tokenIdERC1155
    
    * updated to be optional
    
    * updated naming and tests

--- src/core/token/ERC1155Core.sol
+++ src/core/token/ERC1155Core.sol
@@ -15,6 +15,7 @@ import {BeforeMintCallbackERC1155} from "../../callback/BeforeMintCallbackERC115
 import {BeforeMintWithSignatureCallbackERC1155} from "../../callback/BeforeMintWithSignatureCallbackERC1155.sol";
 import {BeforeTransferCallbackERC1155} from "../../callback/BeforeTransferCallbackERC1155.sol";
 import {UpdateMetadataCallbackERC1155} from "../../callback/UpdateMetadataCallbackERC1155.sol";
+import {UpdateTokenIdCallbackERC1155} from "../../callback/UpdateTokenIdERC1155.sol";
 
 import {OnTokenURICallback} from "../../callback/OnTokenURICallback.sol";
 
@@ -142,7 +143,7 @@ contract ERC1155Core is ERC1155, Core, Multicallable, EIP712 {
         override
         returns (SupportedCallbackFunction[] memory supportedCallbackFunctions)
     {
-        supportedCallbackFunctions = new SupportedCallbackFunction[](8);
+        supportedCallbackFunctions = new SupportedCallbackFunction[](9);
         supportedCallbackFunctions[0] = SupportedCallbackFunction({
             selector: BeforeMintCallbackERC1155.beforeMintERC1155.selector,
             mode: CallbackMode.REQUIRED
@@ -173,6 +174,10 @@ contract ERC1155Core is ERC1155, Core, Multicallable, EIP712 {
             selector: UpdateMetadataCallbackERC1155.updateMetadataERC1155.selector,
             mode: CallbackMode.REQUIRED
         });
+        supportedCallbackFunctions[8] = SupportedCallbackFunction({
+            selector: UpdateTokenIdCallbackERC1155.updateTokenIdERC1155.selector,
+            mode: CallbackMode.OPTIONAL
+        });
     }
 
     /*//////////////////////////////////////////////////////////////
@@ -201,13 +206,14 @@ contract ERC1155Core is ERC1155, Core, Multicallable, EIP712 {
         external
         payable
     {
+        uint256 tokenIdToMint = _updateTokenId(tokenId);
         if (bytes(baseURI).length > 0) {
-            _updateMetadata(to, tokenId, amount, baseURI);
+            _updateMetadata(to, tokenIdToMint, amount, baseURI);
         }
-        _beforeMint(to, tokenId, amount, data);
+        _beforeMint(to, tokenIdToMint, amount, data);
 
-        _totalSupply[tokenId] += amount;
-        _mint(to, tokenId, amount, "");
+        _totalSupply[tokenIdToMint] += amount;
+        _mint(to, tokenIdToMint, amount, "");
     }
 
     /**
@@ -236,13 +242,14 @@ contract ERC1155Core is ERC1155, Core, Multicallable, EIP712 {
             )
         ).recover(signature);
 
+        uint256 tokenIdToMint = _updateTokenId(tokenId);
         if (bytes(baseURI).length > 0) {
-            _updateMetadata(to, tokenId, amount, baseURI);
+            _updateMetadata(to, tokenIdToMint, amount, baseURI);
         }
-        _beforeMintWithSignature(to, tokenId, amount, data, signer);
+        _beforeMintWithSignature(to, tokenIdToMint, amount, data, signer);
 
-        _totalSupply[tokenId] += amount;
-        _mint(to, tokenId, amount, "");
+        _totalSupply[tokenIdToMint] += amount;
+        _mint(to, tokenIdToMint, amount, "");
     }
 
     /**
@@ -391,6 +398,20 @@ contract ERC1155Core is ERC1155, Core, Multicallable, EIP712 {
             UpdateMetadataCallbackERC1155.updateMetadataERC1155.selector,
             abi.encodeCall(UpdateMetadataCallbackERC1155.updateMetadataERC1155, (to, tokenId, amount, baseURI))
         );
+    }
+
+    /// @dev Calls the updateTokenId hook, if installed.
+    function _updateTokenId(uint256 tokenId) internal virtual returns (uint256 tokenIdToMint) {
+        (bool success, bytes memory returndata) = _executeCallbackFunction(
+            UpdateTokenIdCallbackERC1155.updateTokenIdERC1155.selector,
+            abi.encodeCall(UpdateTokenIdCallbackERC1155.updateTokenIdERC1155, (tokenId))
+        );
+        if (success) {
+            tokenIdToMint = abi.decode(returndata, (uint256));
+        } else {
+            // this will only occur when the callback is not implemented
+            tokenIdToMint = tokenId;
+        }
     }
 
     /// @dev Returns the domain name and version for EIP712.
---
+++ test/module/tokenId/sequentialTokenIdERC1155.t.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "lib/forge-std/src/console.sol";
+
+import {OwnableRoles} from "@solady/auth/OwnableRoles.sol";
+import {ERC20} from "@solady/tokens/ERC20.sol";
+import {Test} from "forge-std/Test.sol";
+
+// Target contract
+
+import {Core} from "src/Core.sol";
+import {Module} from "src/Module.sol";
+
+import {Role} from "src/Role.sol";
+import {ERC1155Core} from "src/core/token/ERC1155Core.sol";
+
+import {ICore} from "src/interface/ICore.sol";
+import {IModuleConfig} from "src/interface/IModuleConfig.sol";
+
+import {BatchMetadataERC1155} from "src/module/token/metadata/BatchMetadataERC1155.sol";
+import {BatchMetadataERC721} from "src/module/token/metadata/BatchMetadataERC721.sol";
+import {MintableERC1155} from "src/module/token/minting/MintableERC1155.sol";
+import {SequentialTokenIdERC1155} from "src/module/token/tokenId/sequentialTokenIdERC1155.sol";
+
+contract MockCurrency is ERC20 {
+
+    function mintTo(address _recipient, uint256 _amount) public {
+        _mint(_recipient, _amount);
+    }
+
+    /// @dev Returns the name of the token.
+    function name() public view virtual override returns (string memory) {
+        return "MockCurrency";
+    }
+
+    /// @dev Returns the symbol of the token.
+    function symbol() public view virtual override returns (string memory) {
+        return "MOCK";
+    }
+
+}
+
+contract MintableERC1155Test is Test {
+
+    ERC1155Core public core;
+
+    MintableERC1155 public mintableModule;
+    BatchMetadataERC1155 public batchMetadataModule;
+    SequentialTokenIdERC1155 public sequentialTokenIdModule;
+
+    uint256 ownerPrivateKey = 1;
+    address public owner;
+
+    address tokenRecipient = address(0x123);
+
+    MintableERC1155.MintSignatureParamsERC1155 public mintRequest;
+
+    // Constants
+    address private constant NATIVE_TOKEN_ADDRESS = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    function setUp() public {
+        owner = vm.addr(ownerPrivateKey);
+
+        address[] memory modules;
+        bytes[] memory moduleData;
+
+        core = new ERC1155Core("test", "TEST", "", owner, modules, moduleData);
+        mintableModule = new MintableERC1155();
+        batchMetadataModule = new BatchMetadataERC1155();
+        sequentialTokenIdModule = new SequentialTokenIdERC1155();
+
+        // install module
+        bytes memory encodedInstallParams = abi.encode(owner);
+        vm.prank(owner);
+        core.installModule(address(mintableModule), encodedInstallParams);
+
+        vm.prank(owner);
+        core.installModule(address(batchMetadataModule), "");
+
+        vm.prank(owner);
+        core.installModule(address(sequentialTokenIdModule), "");
+
+        // Give permissioned actor minter role
+        vm.prank(owner);
+        core.grantRoles(owner, Role._MINTER_ROLE);
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                        Tests: beforeMintERC1155
+    //////////////////////////////////////////////////////////////*/
+
+    function test_state_updateTokenId() public {
+        assertEq(SequentialTokenIdERC1155(address(core)).getNextTokenId(), 0);
+
+        // increments the tokenId
+        vm.prank(owner);
+        core.mint(owner, type(uint256).max, 10, "", "");
+
+        assertEq(SequentialTokenIdERC1155(address(core)).getNextTokenId(), 1);
+
+        // does not increment the tokenId
+        vm.prank(owner);
+        core.mint(owner, 1, 10, "", "");
+
+        assertEq(SequentialTokenIdERC1155(address(core)).getNextTokenId(), 1);
+    }
+
+    function test_revert_updateTokenId() public {
+        vm.expectRevert(SequentialTokenIdERC1155.SequentialTokenIdInvalidTokenId.selector);
+        vm.prank(owner);
+        core.mint(owner, 2, 1, "", "");
+    }
+
+}
