ae1cd60a4830f923a8fdc143759b229429ae302b
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-09-04 20:48:52 -0400

    renamed from core to coreInitializable for the ERC1155 (#152)

--- src/core/token/ERC1155CoreInitializable.sol
+++ src/core/token/ERC1155CoreInitializable.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {ERC1155Base} from "./ERC1155Base.sol";
 import {Initializable} from "@solady/utils/Initializable.sol";
 
-contract ERC1155Core is ERC1155Base, Initializable {
+contract ERC1155CoreInitializable is ERC1155Base, Initializable {
 
     constructor() {
         _disableInitializers();
