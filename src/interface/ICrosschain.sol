15052bc31f3b300e466b62f4162403048db41793
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-09-18 13:00:33 -0700

    created unified interface for crosschain transactions (#169)
    * created unified interface for crosschain transactions
    
    * added interface functionality for setter and getter of bridge
    
    * Update implementation
    
    - implemented `sendCrossChainPayloadWithToken`
    - implemented `sendCrossChainPayload`
    - implemented `setRouter`
    - implemented `getRouter`
    - implemented `onMessageSent`
    - implemented `onMessageReceived`
    
    * updated to unify to just one
    
    * FIX: PR updates

---
+++ src/interface/ICrosschain.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.20;
+
+interface ICrosschain {
+
+    /**
+     * @notice Sends a cross-chain transaction.
+     * @param _destinationChain The destination chain ID.
+     * @param _callAddress The address of the contract on the destination chain.
+     * @param _payload The payload to send to the destination chain.
+     * @param _extraArgs The extra arguments to pass
+     * @dev extraArgs may contain items such as token, amount, feeTokenAddress, receipient, gasLimit, etc
+     */
+    function sendCrossChainTransaction(
+        uint64 _destinationChain,
+        address _callAddress,
+        bytes calldata _payload,
+        bytes calldata _extraArgs
+    ) external payable;
+
+    /**
+     * @notice callback function for when a cross-chain transaction is sent.
+     * @param _destinationChain The destination chain ID.
+     * @param _callAddress The address of the contract on the destination chain.
+     * @param _payload The payload sent to the destination chain.
+     * @param _extraArgs The extra arguments sent to the callAddress on the destination chain.
+     */
+    function onCrossChainTransactionSent(
+        uint64 _destinationChain,
+        address _callAddress,
+        bytes calldata _payload,
+        bytes calldata _extraArgs
+    ) internal;
+
+    /**
+     * @notice callback function for when a cross-chain transaction is received.
+     * @param _sourceChain The source chain ID.
+     * @param _sourceAddress The address of the contract on the source chain.
+     * @param _payload The payload sent to the destination chain.
+     * @param _extraArgs The extra arguments sent to the callAddress on the destination chain.
+     */
+    function onCrossChainTransactionReceived(
+        uint64 _sourceChain,
+        address _sourceAddress,
+        bytes calldata _payload,
+        bytes calldata _extraArgs
+    ) internal;
+
+    function setRouter(address _router) external;
+    function getRouter() external view returns (address);
+
+}
