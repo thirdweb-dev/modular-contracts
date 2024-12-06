9d2215992d8b1217713bf1ff150e7e3a8b0ae0a1
Aditya Arora <arora.aditya520@gmail.com>
2024-12-06 12:27:50 -0500

    chore(modular-contracts)-add-pyth-demo (#188)
    * chore(modular-contracts)-add-pyth-demo
    
    * requested changes
    
    * update

--- src/module/PythOracle.sol
+++ src/module/PythOracle.sol
@@ -9,7 +9,7 @@ import "@pythnetwork/pyth-sdk-solidity/PythStructs.sol";
 
 library PythOracleStorage {
 
-    /// @custom:storage-location erc7201:token.minting.mintable
+    /// @custom:storage-location erc7201:pyth.oracle
     bytes32 public constant PYTH_ORACLE_STORAGE_POSITION =
         keccak256(abi.encode(uint256(keccak256("pyth.oracle")) - 1)) & ~bytes32(uint256(0xff));
 
@@ -28,17 +28,18 @@ library PythOracleStorage {
 
 contract PythOracle is Module {
 
-    address immutable pythContract;
+    IPyth immutable pyth;
 
     constructor(address _pythContract) {
-        pythContract = _pythContract;
+        pyth = IPyth(_pythContract);
     }
 
     function getModuleConfig() external pure override returns (ModuleConfig memory config) {
         config.callbackFunctions = new CallbackFunction[](0);
-        config.fallbackFunctions = new FallbackFunction[](1);
+        config.fallbackFunctions = new FallbackFunction[](2);
 
-        fallbackFunctions[0] = FallbackFunction({selector: this.exampleMethod.selector, permissionBits: 0});
+        config.fallbackFunctions[0] = FallbackFunction({selector: this.fetchPythPrices.selector, permissionBits: 0});
+        config.fallbackFunctions[1] = FallbackFunction({selector: this.updatePriceFeeds.selector, permissionBits: 0});
 
         config.registerInstallationCallback = true;
     }
@@ -56,18 +57,25 @@ contract PythOracle is Module {
 
     function encodeBytesOnUninstall() external pure returns (bytes memory) {}
 
-    function exampleMethod(bytes[] calldata priceUpdate) public payable {
+    function updatePriceFeeds(bytes[] calldata priceUpdate) public payable {
+        uint256 fee = pyth.getUpdateFee(priceUpdate);
+        pyth.updatePriceFeeds{value: fee}(priceUpdate);
+    }
+
+    /// @notice fetchPythPrices method to update and read the latest price from a price feed.
+    /// @dev Make sure to send priceUpdates for all priceFeedIds to get the latest price.
+    /// @param priceFeedIds The price feed IDs to update.
+    /// @param priceUpdates The price updates to submit.
+    function fetchPythPrices(bytes32[] calldata priceFeedIds, bytes[] calldata priceUpdates, uint256 maxAge) public payable {
         // Submit a priceUpdate to the Pyth contract to update the on-chain price.
         // Updating the price requires paying the fee returned by getUpdateFee.
         // WARNING: These lines are required to ensure the getPriceNoOlderThan call below succeeds. If you remove them, transactions may fail with "0x19abf40e" error.
-        uint256 fee = pyth.getUpdateFee(priceUpdate);
-        pyth.updatePriceFeeds{value: fee}(priceUpdate);
+        updatePriceFeeds(priceUpdates);
 
-        // Read the current price from a price feed if it is less than 60 seconds old.
-        // Each price feed (e.g., ETH/USD) is identified by a price feed ID.
+        // Read the latest price from a price feed if it is less than 60 seconds old.
         // The complete list of feed IDs is available at https://pyth.network/developers/price-feed-ids
-        bytes32 priceFeedId = 0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace; // ETH/USD
-        PythStructs.Price memory price = pyth.getPriceNoOlderThan(priceFeedId, 60);
+        for (uint32 i = 0; i < priceFeedIds.length; i++) {
+            PythStructs.Price memory price = pyth.getPriceNoOlderThan(priceFeedIds[i], maxAge);
+        }
     }
-
 }
